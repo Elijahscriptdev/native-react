66efcfea1e74f0252d275a36d6489842b1790d92
Elijahscriptdev <elijahobominuru@gmail.com>
2021-03-24 00:35:02 +0100

    play around navigation

--- App.js
+++ App.js
@@ -1,26 +1,61 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  const [text, setText] = useState(
-    "Open up App.js to start working on your app!"
-  );
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
+import ImageScreen from './src/screens/ImageScreen';
 
-  return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title='CHANGE TEXT' onPress={() => setText('changed and woking')} />
-      <StatusBar style='auto' />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
   },
-});
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'App',
+    },
+  }
+);
+
+export default createAppContainer(navigator);
+
+// import { StatusBar } from "expo-status-bar";
+// import React, { useState } from "react";
+// import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+
+// export default function App() {
+//   const [enteredGoal, setEnteredGoal] = useState("");
+
+//   return (
+//     <View style={styles.screen}>
+//       <View style={styles.inputContainer}>
+//         <TextInput placeholder='Course Goal' style={styles.input} />
+//         <Button title='ADD' />
+//       </View>
+//       <View />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   screen: {
+//     padding: 50,
+//   },
+//   inputContainer: {
+//     // flex: 1,
+//     // backgroundColor: "#fff",
+//     // alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   input: {
+//     padding: 5,
+//     // borderBottomColor: "black",
+//     borderBottomWidth: 3,
+//     marginBottom: 10,
+//     borderColor: "red"
+//   },
+// });
