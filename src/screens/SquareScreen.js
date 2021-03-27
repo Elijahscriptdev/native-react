aebf993dc5c35e7a43b391c71c0e6f3bfcc28d45
Elijahscriptdev <elijahobominuru@gmail.com>
2021-03-27 17:17:44 +0100

    add validation to prevent negative color numbers

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,37 +7,54 @@ const COLOR_FIXED = 20;
 const reducer = (state, action) => {
   switch (action.colorToChange) {
     case "red":
-        return { ...state, red: state.red + action.amount};
-      case "green":
-        return { ...state, green: state.green + action.amount};
-      case "blue":
-        return { ...state, blue: state.blue + action.amount};
-      default:
-        return;
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : { ...state, red: state.red + action.amount };
+    case "green":
+      return state.green + action.amount > 255 || state.green + action.amount < 0
+        ? state
+        : { ...state, green: state.green + action.amount };
+    case "blue":
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        ? state
+        : { ...state, blue: state.blue + action.amount };
+    default:
+      return;
   }
-}
+};
 
 const SquareScreen = () => {
-
-  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
   const { red, green, blue } = state;
 
   return (
     <View>
       <Text>Square Screen</Text>
       <ColorCounter
-        onIncrease={() => dispatch({ colorToChange: "red",  amount: COLOR_FIXED })}
-        onDecrease={() => dispatch({ colorToChange: "red",  amount: -1 * COLOR_FIXED })}
+        onIncrease={() =>
+          dispatch({ colorToChange: "red", amount: COLOR_FIXED })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "red", amount: -1 * COLOR_FIXED })
+        }
         color='Red'
       />
       <ColorCounter
-        onIncrease={() => dispatch({ colorToChange: "green", amount:  COLOR_FIXED})}
-        onDecrease={() => dispatch({ colorToChange: "green", amount:  -1 * COLOR_FIXED })}
+        onIncrease={() =>
+          dispatch({ colorToChange: "green", amount: COLOR_FIXED })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "green", amount: -1 * COLOR_FIXED })
+        }
         color='Green'
       />
       <ColorCounter
-        onIncrease={() => dispatch({ colorToChange: "blue", amount:  COLOR_FIXED })}
-        onDecrease={() => dispatch({ colorToChange: "blue", amount:  -1 * COLOR_FIXED })}
+        onIncrease={() =>
+          dispatch({ colorToChange: "blue", amount: COLOR_FIXED })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "blue", amount: -1 * COLOR_FIXED })
+        }
         color='Blue'
       />
       <View
